8afab5ca66c566f26e57804546ed4a74c464af55
dani <groddpapyrus@yahoo.com>
2020-02-25 18:10:03 +0500

    swap app

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwapApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter first number: ");
+            a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter second number: ");
+            b = int.Parse(Console.ReadLine());
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
